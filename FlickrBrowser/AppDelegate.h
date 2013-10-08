3128ee2ea99018e7f415dab2aace4406d8d7083a
admin <admin@imac-125b1b.formation.m2i>
2013-10-08 16:50:33 +0200

    Initial Commit

---
+++ FlickrBrowser/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FlickrBrowser
+//
+//  Created by admin on 08/10/13.
+//  Copyright (c) 2013 Aurelien Fontaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
